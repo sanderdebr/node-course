66d75ee84afd55d20f1b0e7d69c32d82cacfe973
sanderdebr <sander606@gmail.com>
2021-06-07 09:05:53 +0200

    update

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -46,7 +46,6 @@ app.get("/weather", (req, res) => {
   if (!req.query.address) {
     return res.send({ error: "You must provide an address" });
   }
-
   geocode(
     req.query.address,
     (error, { latitude, longitude, location } = {}) => {
