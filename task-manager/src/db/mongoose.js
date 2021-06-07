c8dcf533c0f5e870d915524bab42103b0b994889
sanderdebr <sander606@gmail.com>
2021-06-07 16:59:27 +0200

    mongodb

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -6,19 +7,29 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 });
 
 const Task = mongoose.model("Task", {
-  description: { type: String },
-  completed: { type: Boolean },
+  description: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("this is no email");
+      }
+    },
+  },
+  completed: {
+    type: Boolean,
+  },
 });
 
 const task = new Task({
-  description: "Learn Mongoose",
+  description: "12345",
   completed: false,
 });
 
 task
   .save()
-  .then((me) => {
-    console.log(me);
+  .then((task) => {
+    console.log(task);
   })
   .catch((error) => {
     console.log(error);
